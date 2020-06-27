0fc273d9676a3ba79d8a90a11655cc411871fd25
rjcculaway <rcculaway@up.edu.ph>
2020-06-27 19:40:23 +0800

    Added Biting area

--- project-revive-exer02/src/views/Home.vue
+++ project-revive-exer02/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home col">
+    <div class="my-3" id="bite-area">
+      <textarea class="w-100 p-2 my-1 mx-n1 border-0" id="bite-input" placeholder="What have you been eating?"></textarea>
+      <b-button pill class="primary my-1" type="submit" size="lg" variant="primary">Bite!</b-button>
+    </div>
+    <div class="bits-list border-top">
+      <Bite></Bite>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Bite from '@/components/Bite.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Bite
   }
 }
 </script>
